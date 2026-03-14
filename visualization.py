dc54c1817d09366263faaeabce7c2338d07154c4
yprisha7-svg <yprisha7@gmail.com>
2026-03-14 22:44:44 +0530

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -1,1 +1,21 @@
-
+import graphviz as gph
+def nfa(states, transitions):
+  graph = gph.Diagraph()
+  for s in states:
+    graph.node(s)
+  for key in transtions:
+    start = key[0]
+    symbol = key[1]
+    for end in tansitions[key]:
+      graph.edge("start, end, label = symbol)
+  return g
+def dfa(dfa_states, dfa_trans):
+    graph = gph.Digraph()
+    for s in dfa_states:
+        graph.node(str(s))
+    for key in dfa_trans:
+        start = str(list(key[0]))
+        symbol = key[1]
+        end = str(dfa_trans[key])
+        graph.edge(start, end, label=symbol)
+    return graph
